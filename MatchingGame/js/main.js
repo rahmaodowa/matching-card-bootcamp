9ff95988828d4f7b5f922801c95ecee5806ad96a
rahmaodowa <69065069+rahmaodowa@users.noreply.github.com>
2020-09-14 06:05:04 -0400

    Add files via upload

---
+++ MatchingGame/js/main.js
@@ -0,0 +1,70 @@
+let Match = {
+  restart: ()=>{},
+  removeEvents:()=>{
+    document.querySelectorAll('section').forEach(x=>x.removeEventListener("click", Match.flip))
+  },
+  addEvents: ()=>{
+    document.querySelectorAll('section').forEach(x=>x.addEventListener("click", Match.flip))
+  },
+  choices: [],
+  randomArray: [],
+  randomArray2: [],
+  cardsArray: [],
+
+  randomize: ()=>{
+    Match.cardsArray=[]
+    Match.randomArray=[]
+    Match.randomArray2=[]
+    while(Match.randomArray.length<6){
+      Match.randomArray.push(Math.floor(Math.random()*6))
+      Match.randomArray = [...new Set(Match.randomArray)]
+    }
+    while(Match.randomArray2.length<6){
+      Match.randomArray2.push(Math.floor(Math.random()*6))
+      Match.randomArray2 = [...new Set(Match.randomArray2)]
+    }
+    Match.cardsArray = Match.randomArray.concat(Match.randomArray2)
+    Match.cardsArray.forEach((x,i)=>{document.querySelectorAll('section')[i].classList=`card${x} flip`})
+    console.log(Match.cardsArray)
+  },
+  flipped: [0,0,0,0,0,0,0,0,0,0,0,0],
+  flip: (e)=>{
+    // e.target.style.backgroundImage==='url("https://placekitten.com/200/200")'
+    if(!Match.flipped[e.target.id]){
+    Match.removeEvents()
+      Match.choices.push(e.target.id)
+      console.log(Match.choices)
+      // e.target.classList.toggle(`${e.target.id}`)
+      // e.target.classList.toggle(`${Match.cardsArray[e.target.id]}`)
+      e.target.classList.toggle("flip")
+      Match.flipped[e.target.id] = 1
+
+    if (Match.choices.length===2){
+      Match.check()
+    }else{
+      Match.addEvents()
+    }
+  }
+  },
+  check: ()=>{
+if(Match.cardsArray[Match.choices[0]]!==Match.cardsArray[Match.choices[1]]){
+  setTimeout(()=>{
+  document.querySelectorAll('section')[Match.choices[0]].classList.toggle('flip')
+  document.querySelectorAll('section')[Match.choices[1]].classList.toggle('flip')
+  Match.flipped[Match.choices[0]]=0
+  Match.flipped[Match.choices[1]]=0
+  Match.addEvents()
+  Match.clear()
+},1000)
+}else{
+Match.clear()
+Match.addEvents()
+}},
+
+  clear: ()=>{
+Match.choices = []
+  }
+}
+//============Adds Event Listeners===============//
+Match.randomize()
+Match.addEvents()
